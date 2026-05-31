e83252c0e5fc5c8317d5eb714d257bd9db971d4e
Hugofedoroff <hugo.fedoroff@edu.ece.fr>
2026-05-31 15:18:01 +0200

    modif index

--- backend/index.php
+++ backend/index.php
@@ -9,7 +9,6 @@ require_once __DIR__ . '/middleware/cors.php';
 handleCors();
 header('Content-Type: application/json; charset=utf-8');
  
-// Parse request
 $method = $_SERVER['REQUEST_METHOD'];
 $uri    = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri    = preg_replace('#^/api#', '', $uri);
